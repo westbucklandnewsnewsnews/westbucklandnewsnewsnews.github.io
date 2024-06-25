f3d31cc9433c7fb1134672ab3f9837136779a976
westbucklandnewsnewsnews <westbucklandnewsnewsnews@gmail.com>
2024-06-25 15:50:50 +0100

    Update inject.js

--- test/inject.js
+++ test/inject.js
@@ -1,1 +1,18 @@
+let progress = 0;
+    const progressBar = document.getElementById('progressBar').firstElementChild;
 
+    document.getElementById('yes').addEventListener('click', () => {
+        if (progress < 100) {
+            progress += 10;
+            progressBar.style.width = progress + '%';
+            progressBar.textContent = (progress / 10) + '/10';
+        }
+    });
+
+    document.getElementById('no').addEventListener('click', () => {
+        if (progress < 100) {
+            progress += 10;
+            progressBar.style.width = progress + '%';
+            progressBar.textContent = (progress / 10) + '/10';
+        }
+    });
